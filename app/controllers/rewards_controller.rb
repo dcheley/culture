341dca8e1be7756e6db964abe33716f82658427c
dcheley <danielcheley@hotmail.com>
2017-06-27 13:45:38 -0400

    Creating rewards routes, directory, views and controller for new & create actions

---
+++ app/controllers/rewards_controller.rb
@@ -0,0 +1,26 @@
+class RewardsController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+  end
+
+  def create
+    @reward = Reward.new(reward_params)
+    if @reward.save
+      redirect_to 'users#show', notice: 'Gift card chosen, view your current progress below'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def load_reward
+    @reward = Reward.find(params[:id])
+  end
+
+  def reward_params
+    params.require(:reward).permit(:name, :award, :progress, :user_id,
+    :activity_id)
+  end
+end
