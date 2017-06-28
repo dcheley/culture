e4cecc91123558fdbadb1ed8cde4d90bb988abc1
dcheley <danielcheley@hotmail.com>
2017-06-28 13:19:02 -0400

    Setting optional to true

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,5 +1,5 @@
 class Activity < ApplicationRecord
-  belongs_to :rewards
+  belongs_to :rewards, optional: true
 
   validates :name, presence: true
   validates :reward_id, numericality: { greater_than: 0 }
