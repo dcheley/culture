5b01522e9c74be49327589a5ca2c700a46ab530c
dcheley <danielcheley@hotmail.com>
2017-06-28 14:08:33 -0400

    Adding admin to user strong params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
     if current_user.reward == nil
@@ -7,5 +8,11 @@ class ApplicationController < ActionController::Base
     else
       user_url(current_user)
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:signup, keys: [:admin])
   end
 end
