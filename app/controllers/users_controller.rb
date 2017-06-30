6f7abe65babc46959944c0402d65e981fc981062
dcheley <danielcheley@hotmail.com>
2017-06-30 00:20:17 -0400

    Changing controller variable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @reward = Reward.find_by(user_id: @user.id)
-    @activities = Activity.where(reward_id: @user.id)
+    @activities = Activity.where(reward_id: @user.email)
   end
 end
