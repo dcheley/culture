4ca9f4b37a52a294ba44dd2c45c1137df2b8d873
dcheley <danielcheley@hotmail.com>
2017-06-30 00:17:50 -0400

    Setting email validations

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,6 +2,7 @@ class Activity < ApplicationRecord
   belongs_to :rewards, optional: true
 
   validates :name, presence: true
-  validates :reward_id, numericality: { greater_than: 0 }
+  validates :reward_id, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/, :on => :create, :update }
+  validates :contact, , :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/, :on => :create, :update }
   validates :prize, numericality: { greater_than_or_equal_to: 0 }
 end
