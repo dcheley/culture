b11924108aa8c19eb780cd77e28c01fc52297b7b
dcheley <danielcheley@hotmail.com>
2017-06-30 12:01:29 -0400

    Allowing contact column to be blank & changing where conditions to properly display user activities

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,6 +3,6 @@ class Activity < ApplicationRecord
 
   validates :name, presence: true
   validates :reward_id, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/, :on => [:create, :update] }
-  validates :contact, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/, :on => [:create, :update] }
+  validates :contact, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/, :on => [:create, :update] }, allow_blank: true
   validates :prize, numericality: { greater_than_or_equal_to: 0 }
 end
