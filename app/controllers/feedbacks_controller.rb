1eade4e9a2ddf3fcbe218a746711de3e6ee49e91
dcheley <danielcheley@hotmail.com>
2017-07-06 13:35:21 -0400

    Writing new & create logic & strong params

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,1 +1,21 @@
 class FeedbacksController < ApplicationController
+  def new
+    @feedback = Feedback.new
+  end
+
+  def create
+    @feedback = Feedback.new(feedback_params)
+    if @feedback.save
+      redirect_to user_url(current_user), notice:"Feedback successfully sent"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def feedback_params
+    params.require(:feedback).permit(:first_takeaway, :second_takeaway,
+    :third_takeaway, :fourth_takeaway, :fifth_takeaway)
+  end
+end
