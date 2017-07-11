3ce8bdf8fc60ebfe197ad6d08537306952c28a58
dcheley <danielcheley@hotmail.com>
2017-07-11 14:22:07 -0400

    Allowing new hire column to be blank & precompiling assets

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,7 +3,7 @@ class Activity < ApplicationRecord
   belongs_to :feedback, optional: true
 
   validates :name, presence: true
-  validates :user_id, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/, :on => [:create, :update] }
+  validates :user_id, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/, :on => [:create, :update] }, allow_blank: true
   validates :contact, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/, :on => [:create, :update] }, allow_blank: true
   validates :prize, numericality: { greater_than_or_equal_to: 0 }
 end
