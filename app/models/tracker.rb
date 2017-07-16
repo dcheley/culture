4bef2b134cef155b0e53069409d71841a37a6661
dcheley <danielcheley@hotmail.com>
2017-07-16 16:39:18 -0400

    Changing relationship to One to Many & calling different variables in view

--- app/models/tracker.rb
+++ app/models/tracker.rb
@@ -1,6 +1,6 @@
 class Tracker < ApplicationRecord
   belongs_to :users, optional: true, foreign_key: :user_email, primary_key: :email
-  belongs_to :activities, optional: true
+  belongs_to :activity
   belongs_to :feedback, optional: true
 
   validates :user_email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/, :on => [:create, :update] }
