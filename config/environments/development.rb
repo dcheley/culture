25b72afa99fc3bab9b8884898243188e17b3cb15
dcheley <danielcheley@hotmail.com>
2017-07-17 16:48:28 -0400

    Setting up smtp mailer settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,19 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => "587",
+    :domain => "gmail.com",
+    :user_name => Rails.application.secrets.GMAIL_USERNAME,
+    :password => Rails.application.secrets.GMAIL_PASSWORD,
+    :authentication => "plain",
+    :enable_starttls_auto => true
+  }
+
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
