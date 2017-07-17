dd4977529917af04ea8d90f29a91816470ae86f9
dcheley <danielcheley@hotmail.com>
2017-07-17 14:31:38 -0400

    Setting up employee contact emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
   default from: 'notifications@shape.com'
 
-  def employee_email(user)
+  def employee_activity_email(user)
     @user = user
     @url = 'http://shape-culture.herokuapp.com'
-    mail(to: @user.email, subject: 'Shape Notification')
+    mail(to: @user.email, subject: 'New Shape Activity')
   end
 end
